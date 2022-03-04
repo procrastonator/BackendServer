deb3d625e5bf04b5a7881191fd6712b209c6b0bd
Marten Zand-Vakili <martenzv@hotmail.de>
2022-03-04 14:36:34 +0100

    Thy routes are now proceding to work

--- routes/goal.routes.js
+++ routes/goal.routes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 // const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const Goal = require("../models/Goal.model");
 const Project = require("../models/Project.model");
+
+
+
 
 router.post("/", (req, res) => {
   
@@ -30,5 +33,44 @@ router.post("/", (req, res) => {
       });
     });
 });
+
+
+router.get("/", (req, res, next ) => {
+  Goal.find()
+  .then((allGoals) => res.json(allGoals))
+  .catch((err) => res.json(err))
+});
+
+router.get("/:goalId", (req, res, next) => {
+  const {goalId} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+  Goal.findById(goalId)
+  .then((goal) => res.json(goal))
+  .catch((err) => res.status(500).json(err));
+})
+
+
+router.put("/:goalId/update", (req, res, next) => {
+  const { goalId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(goalId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  const goalDetails = {
+    title: req.body.title,
+    description: req.body.description
+  };
+
+  Goal.findByIdAndUpdate(goalId, goalDetails, { new: true })
+  .then((updatedGoal) => res.json(updatedGoal))
+  .catch((error) => res.json(error));
+});
+
 
 module.exports = router;
